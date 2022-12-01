34572a72808c241cd1e3377df18024f9a3ab78c4
natistuomi <77719270+natistuomi@users.noreply.github.com>
2022-12-01 13:38:46 +0100

    Day 1

--- src/Answers.java
+++ src/Answers.java
@@ -1,6 +1,6 @@
 public class Answers {
     public static void main(String [] args){
-        System.out.println("DAY 1");
+        System.out.println("DAY 1 - Calories");
         Calories a = new Calories("input_calories.txt", 2235);
         System.out.println("Largest amount of calories: " + a.getMostCalories());
         System.out.println("Total calories of top three elves: " + a.getTotalTopThreeCalories());
